4f3cd3226898f2de96d9f579498396fcc456d0a0
cogaoqiusheng <15105011156@163.com>
2019-03-15 16:19:02 +0800

    初始化项目

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,85 @@
+'use strict';
+const path = require('path');
+const config = require('../config');
+const utils = require('./utils');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+const devMode = process.env.NODE_ENV !== 'production';
+// console.log(process.env.NODE_ENV);
+function resolve(dir) {
+    return path.join(__dirname, '..', dir);
+}
+
+module.exports = {
+    context: path.resolve(__dirname, '../'), // 设置基路径
+    entry: {
+        app: './src/main.js'
+    },
+    output: {
+        path: config.build.assetsRoot,
+        filename: '[name].js',
+        publicPath: process.env.NODE_ENV === 'production'
+            ? config.build.assetsPublicPath
+            : config.dev.assetsPublicPath   //  配置外部文件（图片）和 按需加载文件的路径
+    },
+    resolve: {
+        extensions: ['.js', '.vue', '.json', '.less'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '@': resolve('src'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath('img/[name].[hash:7].[ext]')
+                }
+            },
+            {
+                test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath('media/[name].[hash:7].[ext]')
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+                }
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    devMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+                    {
+                    loader: 'css-loader' // translates CSS into CommonJS
+                }, {
+                    loader: 'less-loader' // compiles Less to CSS
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+
+}
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view/> -->
+    hello I'm vue123
+    {{this.name}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+      return {
+          name: 'gaosen'
+      }
+  },
+}
+</script>
+
+<style lang="less">
+// @import url('./styles/base.less');
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
