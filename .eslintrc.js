3aa82ebd41fcc42e273f41f08d414b6caba3edb6
cogaoqiusheng <15105011156@163.com>
2019-03-21 10:06:26 +0800

    配置eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",  // 一个配置文件可以被基础配置中的已启用的规则继承
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,   // 指定年份
+        "sourceType": "module",  // 代码为ECMAScript模块
+        "requrie": false,
+        "exports": false
+    },
+    "rules": {
+        "eqeqeq": 1,
+        "no-console": 0,
+        
+    }
+};
